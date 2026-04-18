3dc3d74915d81d1cc637355b401fb2a5b08dcdb2
dooms <doomspire.us@gmail.com>
2026-04-17 20:53:19 -0400

    initial upload to github

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    application
+    kotlin("jvm") version "2.3.10"
+    id("io.github.goooler.shadow") version "8.1.8"
+}
+
+group = "me.jonathankrzeszewski"
+val jarVersion = "0.0.1"
+version = jarVersion
+
+repositories { mavenCentral() }
+dependencies {
+    implementation("com.formdev:flatlaf:3.7.1")
+    implementation("ch.qos.logback:logback-classic:1.5.32")
+}
+
+kotlin { jvmToolchain(21) }
+
+application {
+    mainClass.set("$group.MainKt")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set(rootProject.name)
+    archiveVersion.set(jarVersion)
+    archiveClassifier.set("")
+
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package me.jonathankrzeszewski
+
+fun main() {
+    println("app")
+}
